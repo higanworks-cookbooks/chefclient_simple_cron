f7c40553bdd80c62d80515b04a3a3948802e73e0
sawanoboly <sawanoboriyu@higanworks.com>
2013-05-30 12:31:31 +0900

    bump 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sawanoboriyu@higanworks.com'
 license          'apache2'
 description      'just add chef-client to crontab, for smartos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-supports         'smartos'
+version          '0.2.0'
+supports         'smartos','ubuntu'
 depends          'logadm'
